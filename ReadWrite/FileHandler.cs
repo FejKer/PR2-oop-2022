ef91d7c192d7bec9f9aea6e3d0f77eee5a240d5f
Oskar Migowski <o.migowski@protonmail.com>
2022-11-12 14:06:09 +0100

    Add files via upload

---
+++ ReadWrite/FileHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class FileHandler
+{
+	public FileHandler()
+	{
+	}
+
+	public static bool Save(string filename, List<String> list)
+	{
+		FileStream fsWrite = new FileStream(filename, FileMode.OpenOrCreate);
+
+        if (!fsWrite.CanWrite)
+        {
+			return false;
+        }
+
+		StreamWriter sw = new StreamWriter(fsWrite);
+		sw.AutoFlush = true;
+
+		foreach(string item in list)
+        {
+			sw.WriteLine(item);
+		}
+
+		fsWrite.Close();
+
+		return true;
+	}
+	public static List<String> Read(string filename)
+    {
+		List<string> list = new List<string>();
+		FileStream fsRead = new FileStream(filename, FileMode.Open);
+		StreamReader sr = new StreamReader(fsRead);
+
+        while (!sr.EndOfStream)
+        {
+			list.Add(sr.ReadLine());
+        }
+		return list;
+    }
+}
---
+++ ReadWrite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("jacek");
+            list.Add("wacek");
+            list.Add("placek");
+            list.Add("gacek");
+            list.Add("pałko");
+            FileHandler.Save("C:\\Users\\ozzy\\Desktop\\file.txt", list);
+            foreach(string item in FileHandler.Read("C:\\Users\\ozzy\\Desktop\\file.txt"))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
